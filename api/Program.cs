0db382f65e1daf30d4e190d2ec0b78b8c2294cc3
wsjoden <sjoden.william@gmail.com>
2026-09-24 13:53:35 +0200

    swagger

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Pulse.Api.Data;
+using Pulse.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configure database
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
+
+
+builder.Services.AddControllers();
+// Confgiure Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -16,11 +24,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapGroup("/api/quiz").MapQuizEndpoints();
 app.UseHttpsRedirection();
-
-
 
 app.Run();
 
